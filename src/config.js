62857f34c743a075be597b47a8e75dc04e4f2474
Vladyslav <vladka@webdevelop.pro>
2017-11-06 20:59:45 +0200

    Fixed config for facebook

--- src/config.js
+++ src/config.js
@@ -14,9 +14,9 @@ var e = {
   teamTitle: "Meet The Team",
   siteTitle: 'Crowdfunding | GrowthFountain Equity Crowdfunding',
 
-  facebookClientId : "547857385398592",
-  googleClientId : "372921150-paa9eek64iuo2d5pmg7nraivc72g0sfo.apps.googleusercontent.com",
-  linkedinClientId : "77hgtbs9rkjk20",
+  facebookClientId: "1405768896335643",
+  googleClientId: "805823281871-0sbsf2btjd5j13g5aa7sfo4pfjorn3k2.apps.googleusercontent.com",
+  linkedinClientId: "77wzj6tz0yyr33",
 
   googleMapKey: "AIzaSyBpCl9-7bkVISZ0o-AaFCsKzZwGAxalkZU",
   stripeKey: "pk_test_Z7YAhlyPtnW7bpd8LJUHTSou",
